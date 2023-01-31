2c44ee6e28a433010e08706c5d82215efc862191
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-31 16:42:07 +0100

    Add metodi relazione ai model Technology e Project Aggiunge seeder del model Technology

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Project;
 use App\Models\Type;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
@@ -18,7 +19,10 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        // Metodo truncate() per ripopolare da zero il seeder (Type in questo caso) ogni volta che viene rilanciato
+        Schema::disableForeignKeyConstraints();
         Project::truncate();
+        Schema::enableForeignKeyConstraints();
 
         for ($i = 0; $i < 10; $i++) {
             // prendo una tipologia di progetto in maniera randomica
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +17,28 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Metodo truncate() per ripopolare da zero il seeder (Type in questo caso) ogni volta che viene rilanciato
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $technologies = [
+            'Html',
+            'CSS',
+            'PHP',
+            'VueJS',
+            'Laravel',
+            'JavaScript',
+            'Python',
+            'C++',
+            'Swift',
+        ];
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Str::slug($new_technology->name, '-');
+            $new_technology->save();
+        }
     }
 }
